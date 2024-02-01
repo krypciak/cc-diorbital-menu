fb56f56bf4ef886ce3c18918738acbc82fdbd20d
krypek <krypek@tuta.io>
2024-02-01 20:50:38 +0100

    Add reload button & map reload button

--- src/buttonEntries.ts
+++ src/buttonEntries.ts
@@ -1,4 +1,5 @@
 export function addButtonEntries() {
+    /* character swap */
     function getPlayerHeadConfig(playerName: string): sc.AdditionalRingButton.ImageConfig {
         return () => {
             const headIdx = sc.party.models[playerName].getHeadIdx()
@@ -25,4 +26,29 @@ export function addButtonEntries() {
             }
         }
     })
+
+    if (window.vim) {
+        sc.QuickRingMenu.ringConfigs[0][1] = {
+            name: 'reload',
+            description: 'Reload the game',
+            pressEvent: () => window.vim.executeString('reload'),
+            image: () => ({
+                gfx: new ig.Image('media/gui/menu.png'),
+                srcPos: { x: 449, y: 464 },
+                pos: { x: 9, y: 8 },
+                size: { x: 16, y: 16 },
+            }),
+        }
+        sc.QuickRingMenu.ringConfigs[0][3] = {
+            name: 'reload map',
+            description: 'Reload the map',
+            pressEvent: () => window.vim.executeString('reload-level'),
+            image: () => ({
+                gfx: new ig.Image('media/gui/menu.png'),
+                srcPos: { x: 433, y: 464 },
+                pos: { x: 9, y: 8 },
+                size: { x: 16, y: 16 },
+            }),
+        }
+    }
 }
